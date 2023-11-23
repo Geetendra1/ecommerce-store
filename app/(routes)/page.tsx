721976b80a6dd7f880a2622f6e01ea26d8be729d
= <=>
2023-11-23 16:43:44 +0530

    added bilboard id

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("e0713af5-e048-4628-a8b7-b767771bd1e5");
 
   return (
     <Container>
